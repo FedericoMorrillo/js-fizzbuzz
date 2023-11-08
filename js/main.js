3b9c770bd020631a73264f04e6307e0a3a75bda5
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-08 22:08:16 +0100

    feat: valori dei multipli + colore celle

--- js/main.js
+++ js/main.js
@@ -11,19 +11,27 @@ const elementUl = document.querySelector("ul.list")
 // ciclo per generare i numeri da 1 a 100
 for (let i = 1; i < 101; i++) {
     console.log(i);
-    const elementLi = document.createElement('li')
+
+    // creiamo le liste dentro l' ul nell' html
+    let elementLi = document.createElement('li')
     elementLi.append(i);
     elementUl.append(elementLi);
 
     // condizioni per stampare in console fizz, buzz, fizzbuzz
     if (i % 3 === 0) {
         console.log(fizz);
+        elementLi.innerHTML = fizz;
+        elementLi.style.backgroundColor = "#00D03B";
     }
     if (i % 5 === 0) {
         console.log(buzz)
+        elementLi.innerHTML = buzz;
+        elementLi.style.backgroundColor = "#AEFF00";
     }
     if (i % 3 === 0 && i % 5 === 0) {
         console.log(fizzbuzz)
+        elementLi.innerHTML = fizzbuzz;
+        elementLi.style.backgroundColor = "#C93939";
     }
 
 }
